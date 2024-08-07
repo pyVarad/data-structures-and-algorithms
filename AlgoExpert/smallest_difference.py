7a78c4f32ffcfbbf2cf31b819034e4301f00a017
varad <py.varad@gmail.com>
2024-08-07 08:06:21 +0530

    exercise 3: Added a more optimized solution
    Signed-off-by: varad <py.varad@gmail.com>

--- AlgoExpert/smallest_difference.py
+++ AlgoExpert/smallest_difference.py
@@ -36,12 +36,33 @@ def smallest_difference(array_one: List[int], array_two: List[int]) -> List[int]
 
 
 def smallest_difference_improved_time_complexity(array_one: List[int], array_two: List[int]) -> List[int]:
-    pass
+    array_one.sort()
+    array_two.sort()
+
+    print("Array One:", array_one)
+    print("Array Two:", array_two)
+
+    array_one_ptr, array_two_ptr = 0, 0
+    pairs = []
+    least_diff = float("inf")
+
+    while array_one_ptr < len(array_one) and array_two_ptr < len(array_two):
+        current_sum = abs(array_one[array_one_ptr] - array_two[array_two_ptr])
+        if current_sum < least_diff:
+            pairs = [array_one[array_one_ptr], array_two[array_two_ptr]]
+            least_diff = current_sum
+
+        if array_one[array_one_ptr] > array_two[array_two_ptr]:
+            array_two_ptr += 1
+        else:
+            array_one_ptr += 1
+
+    return pairs
 
 
 if __name__ == "__main__":
-    result = smallest_difference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17])
-    print(result)
+    # result = smallest_difference([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17])
+    # print(result)
 
     result = smallest_difference_improved_time_complexity([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17])
     print(result)
