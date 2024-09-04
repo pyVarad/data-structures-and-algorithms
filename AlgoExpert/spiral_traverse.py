2e21198fd8a83c034caf79b07e6c996fc2c76ef1
varad <py.varad@gmail.com>
2024-09-04 06:29:23 +0530

    Fixed few issues

--- AlgoExpert/spiral_traverse.py
+++ AlgoExpert/spiral_traverse.py
@@ -17,8 +17,10 @@ Sample Output
 -------------
 [1 , 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
 """
+from typing import List
 
-def spiral_traverse(array):
+
+def spiral_traverse(array: List[List[int]]) -> List[int]:
     spiral = []
     if len(array) > 0:
         num_cols = len(array[0])
@@ -28,36 +30,46 @@ def spiral_traverse(array):
         start_col, last_col = 0, num_cols
 
         while start_row <= last_row and start_col <= last_row:
-            for col in range(start_col, last_col + 1):
-                spiral.append(col)
-            start_row += 1
+            for col in range(start_col, last_col - 1):
+                spiral.append(array[start_row][col])
+                print("Inside Col forward iteration: ", start_col, last_col - 1)
 
-            if start_row > last_row:
-                break
+            for row in range(start_row, last_row - 1):
+                spiral.append(array[row][last_col - 1])
+                print("Inside row downward iteration: ", start_row, last_row - 1)
 
-            for row in range(start_row, last_row + 1):
-                spiral.append(row)
-            last_col -= 1
 
-            if start_col > last_col:
-                break
+            for col in range(last_col - 1, start_col, -1):
+                spiral.append(array[last_row - 1][col])
+                print("Inside Col reverse iteration: ", last_col - 1, start_col)
 
-            for col in reversed(range(start_col, last_col+ 1)):
-                spiral.append(col)
+
+            for row in range(last_row - 1, start_row, -1):
+                spiral.append(array[row][start_col])
+                print("Inside row upward iteration: ", last_row - 1, start_row)
+
             last_row -= 1
-
-            for row in reversed(range(start_row, last_row + 1)):
-                spiral.append(row)
+            last_col -= 1
+            start_row += 1
             start_col += 1
+
 
     return spiral
 
+
 if __name__ == "__main__":
     array = [
-        [1, 2, 3, 4],
-        [12 ,13, 14, 5],
-        [11, 16, 15, 6],
-        [10, 9, 8, 7]
-    ]
+            [27, 12, 35, 26],
+            [25, 21, 94, 11],
+            [19, 96, 43, 56],
+            [55, 36, 10, 18],
+            [96, 83, 31, 94],
+            [93, 11, 90, 16]
+        ]
+
     res = spiral_traverse(array)
     print(res)
+
+
+# [27, 12, 35, 26, 11, 56, 18, 94, 16, 90, 11, 93, 96, 55, 19, 25, 21, 94, 43, 10, 31, 83, 36, 96]
+# [27, 12, 35, 26, 11, 56, 18, 94, 31, 83, 96, 55, 19, 25, 21, 94, 43, 10, 36, 96]
